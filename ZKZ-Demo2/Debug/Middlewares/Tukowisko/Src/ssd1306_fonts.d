7ebd98956c25118ec96a09c5c4f1ca13cb29c842
Tuka <82777719+tukowisko@users.noreply.github.com>
2022-01-25 19:23:09 +0100

    Delete ZKZ-Demo2/Debug directory

--- ZKZ-Demo2/Debug/Middlewares/Tukowisko/Src/ssd1306_fonts.d
+++
@@ -1,5 +0,0 @@
-Middlewares/Tukowisko/Src/ssd1306_fonts.o: \
- ../Middlewares/Tukowisko/Src/ssd1306_fonts.c \
- ../Middlewares/Tukowisko/Inc/ssd1306_fonts.h
-
-../Middlewares/Tukowisko/Inc/ssd1306_fonts.h:
